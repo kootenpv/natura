3e268233f2124436597768adba34456a92318ffb
Pascal van Kooten <kootenpv@gmail.com>
2016-08-28 20:32:26 +0200

    reimplemented repr

--- natura/classes.py
+++ natura/classes.py
@@ -38,6 +38,11 @@ class Money():
         self.base_amount = base_amount
         self.last_modified_base = last_modified_base
 
+    def __repr__(self):
+        msg = "{}({})"
+        values = ', '.join(["{}={}".format(x, getattr(self, x)) for x in self.names])
+        return msg.format(self.__class__.__name__, values)
+
     def to_dict(self):
         result = {k: getattr(self, k) for k in self.names}
         result['spans'] = [{"begin": x[0], "end": x[1]} for x in self.spans]
