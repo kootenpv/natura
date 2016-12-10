71e70f4abf8deb5a8d54fbbf33c44b1136566674
Pascal van Kooten <kootenpv@gmail.com>
2016-12-10 23:51:56 +0100

    missing bucks

--- filter_prep.py
+++ filter_prep.py
@@ -2,12 +2,13 @@ import just
 usm = just.read("data/money/us.json")
 
 usd_min = set([x[0] for x in usm['symbols']["$"]] +
-              [x[0] for x in usm['currencies']["dollar"]])
+              [x[0] for x in usm['currencies']["dollar"]] +
+              [x[0] for x in usm['currencies']["bucks"]])
 
 usm['keywords'] = {k: v for k, v in usm['keywords'].items() if v in usd_min}
 usm['symbols'] = {"$": usm['symbols']['$']}
-usm['currencies'] = {"dollar": usm['currencies']['dollar']}
-usm['currencies'] = {"bucks": usm['currencies']['bucks']}
+usm['currencies'] = {"dollar": usm['currencies']['dollar'],
+                     "bucks": usm['currencies']['bucks']}
 usm['abbrevs'] = [x for x in usm['abbrevs'] if x in usm['keywords'].values()]
 
 usm = just.write(usm, "data/money/us_min.json")
